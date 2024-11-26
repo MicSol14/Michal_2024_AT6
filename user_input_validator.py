0cb84a7b59a740f5f31a122733c12efab8bd4770
Michal Soltys <michal.soltys14@gmail.com>
2024-11-26 15:33:49 +0100

    Add display_validation_message and class description

--- user_input_validator.py
+++ user_input_validator.py
@@ -4,4 +4,8 @@ class UserInputValidator:
         for item in input_list:
             if item.isdigit() and int(item) > 0:
                 valid_integers.append(int(item))
+        self.display_validation_message()
         return valid_integers
+
+    def display_validation_message(self):
+        print("The input list has been validated.")
